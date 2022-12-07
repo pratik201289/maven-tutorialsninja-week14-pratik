10ea2a43618340f696b3f32e7a022056b7f02027
pratik201289 <114582321+pratik201289@users.noreply.github.com>
2022-12-07 11:46:02 +0000

    Update laptopsAndNotebooksPage.java

--- src/main/java/com/tutorialsninja/demo/pages/laptopsAndNotebooksPage.java
+++ src/main/java/com/tutorialsninja/demo/pages/laptopsAndNotebooksPage.java
@@ -2,6 +2,9 @@ package com.tutorialsninja.demo.pages;
 
 import com.tutorialsninja.demo.utility.Utility;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.*;
 
 public class laptopsAndNotebooksPage extends Utility {
     By showAllLaptops = By.xpath("//a[text()='Show All Laptops & Notebooks']");
@@ -43,8 +46,38 @@ public class laptopsAndNotebooksPage extends Utility {
         clickOnElement(showAllLaptops);
     }
 
-    public void selectSortByPriceHighToLow() {
-        selectByVisibleTextFromDropDown(priceHighToLow, "Price (High > Low)");
+    public Map<String, ArrayList> selectSortByPriceHighToLow() {
+
+        Map<String, ArrayList> mapArrays = new HashMap<String, ArrayList>();
+
+//        selectByVisibleTextFromDropDown(priceHighToLow, "Price (High > Low)");
+        // Get all the products price and stored into array list
+        List<WebElement> products = driver.findElements(By.xpath("//p[@class ='price']"));
+        List<Double> originalProductsPrice = new ArrayList<>();
+        for (WebElement e : products) {
+            System.out.println(e.getText());
+            String[] arr = e.getText().split("Ex Tax:");
+            originalProductsPrice.add(Double.valueOf(arr[0].substring(1).replaceAll(",","")));
+        }
+        System.out.println(originalProductsPrice);
+        // Sort By Reverse order
+        Collections.sort(originalProductsPrice, Collections.reverseOrder());
+        System.out.println(originalProductsPrice);
+        // Select sort by Price (High > Low)
+        selectByVisibleTextFromDropDown(By.id("input-sort"), "Price (High > Low)");
+        // After filter Price (High > Low) Get all the products name and stored into array list
+        products = driver.findElements(By.xpath("//p[@class ='price']"));
+        ArrayList<Double> afterSortByPrice = new ArrayList<>();
+        for (WebElement e : products) {
+            String[] arr = e.getText().split("Ex Tax:");
+            afterSortByPrice.add(Double.valueOf(arr[0].substring(1).replaceAll(",","")));
+        }
+        System.out.println(afterSortByPrice);
+
+        mapArrays.put("originalProductsPrice", (ArrayList) originalProductsPrice);
+        mapArrays.put("afterSortByPrice", afterSortByPrice);
+        return mapArrays;
+
     }
 
     public void selectProductMacbook() {
